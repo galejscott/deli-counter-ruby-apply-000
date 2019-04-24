5695ef73e9170027fb432e416704f788629be07d
Scott Gale <galejscott@gmail.com>
2019-04-24 17:07:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 katz_deli = []
 
-# show line. if nobody,
+# show line with numbers. if nobody, say so
 def line(array)
   arr = []
   if array.length == 0
@@ -9,28 +9,43 @@ def line(array)
     array.each.with_index(1) do |name, index|
       arr.push("#{index}. #{name}")
     end
-    puts "The line is currently: #{arr.join(" ")}"
+    puts "The line is currently: #{arr.join(" ,")}"
   end
 end
 
 line(katz_deli)
 
 # add to array and number (+1) 
-def take_a_number(array, name)
-  array.push(name)
-  puts "Welcome, #{name}. You are number #{array.length} in line."
+def take_a_number(array, number)
+  number += 1
+  array.push(number)
+  puts "Welcome, you are number #{number}."
+  return number
 end
+
+num = 0
+num = take_a_number(katz_deli, num)
+num = take_a_number(katz_deli, num)
+num = take_a_number(katz_deli, num)
+
+# 1,2,3
+# 1 gets served
+# 2,3
+# 2,3,4
+# 2,3,and 4 get served
+# empty
+# 5
 
 take_a_number(katz_deli, "Ada")
 take_a_number(katz_deli, "Grace")
 take_a_number(katz_deli, "Kent")
 line(katz_deli)
 
-# current servee and remove from array. if nobody
+# current servee and remove from array. if nobody, say so
 def now_serving(array)
-  if array.empty? == true
+  if array.empty?
     puts "There is nobody waiting to be served!"
-  else array.empty? == false
+  else 
     puts "Currently serving #{array[0]}."
     array.shift
   end
